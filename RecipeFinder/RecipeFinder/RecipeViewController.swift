b70d04135db70738123c08cc151d1609b02312e3
Sismanidis, Evangelos (415) <evangelos.sismanidis@daimler.com>
2017-08-12 14:22:58 +0200

    added the tableView for showing the recipes later

--- RecipeFinder/RecipeFinder/RecipeViewController.swift
+++ RecipeFinder/RecipeFinder/RecipeViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class RecipeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class RecipeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView = UITableView()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
         
-        self.view.backgroundColor = UIColor.green
-        self.title = "Recipe Finder"
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        let view = UIView()
+        view.addSubview(tableView)
+        
+        // Normally I would use a Library like Snapkit for setting the Autolayout constraints
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo:view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo:view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+        self.view = view
+        self.title = "RECEIPT FINDER"
+    }
+
+    // MARK: UITableViewDelegate & UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "TEST"
+        return cell
     }
 }
 
