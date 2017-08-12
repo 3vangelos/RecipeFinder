1ee0f738f9f67fa8deb79c7769db34b11596deac
Sismanidis, Evangelos (415) <evangelos.sismanidis@daimler.com>
2017-08-12 14:14:48 +0200

    - Added paragraph about used API - Removed Storyboard - Setup View Hierarchy with NavigationController

---
+++ RecipeFinder/RecipeFinder/RecipeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeFinder
+//
+//  Created by Sismanidis, Evangelos (415) on 13.05.17.
+//  Copyright © 2017 Private. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.green
+        self.title = "Recipe Finder"
+    }
+}
+
